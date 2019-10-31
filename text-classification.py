eda0c33e4a6b9bbb183d6922d7c28e119b723e1f
lutvianes <lutvianes@gmail.com>
2019-10-31 23:58:31 +0700

    Add tokenizer and cleaner function

--- text-classification.py
+++ text-classification.py
@@ -7,3 +7,49 @@ from sklearn.pipeline import Pipeline
 df_amazon = pd.read_csv ("datasets/amazon_alexa.tsv", sep="\t")
 
 print(df_amazon.head())
+
+import spacy
+import string
+from spacy.lang.en.stop_words import STOP_WORDS
+from spacy.lang.en import English
+
+# Create our list of punctuation marks
+punctuations = string.punctuation
+
+# Create our list of stopwords
+nlp = spacy.load('en')
+stop_words = spacy.lang.en.stop_words.STOP_WORDS
+
+# Load English tokenizer, tagger, parser, NER and word vectors
+parser = English()
+
+# Creating our tokenizer function
+def spacy_tokenizer(sentence):
+    # Creating our token object, which is used to create documents with linguistic annotations.
+    mytokens = parser(sentence)
+
+    # Lemmatizing each token and converting each token into lowercase
+    mytokens = [ word.lemma_.lower().strip() if word.lemma_ != "-PRON-" else word.lower_ for word in mytokens ]
+
+    # Removing stop words
+    mytokens = [ word for word in mytokens if word not in stop_words and word not in punctuations ]
+
+    # return preprocessed
+    return mytokens # list of tokens
+
+# Custom transformer using spaCy
+class predictors(TransformerMixin):
+    def transform(self, X, **transform_params):
+        # Cleaning Text
+        return [clean_text(text) for text in X]
+
+    def fit(self, X, y=None, **fit_params):
+        return self
+
+    def get_params(self, deep=True):
+        return {}
+
+# Basic function to clean the text
+def clean_text(text):
+    # Removing spaces and converting text into lowercase
+    return text.strip().lower()
