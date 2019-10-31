42122de4718d3c6335eb6883cd8e1381e7679d72
lutvianes <lutvianes@gmail.com>
2019-10-31 23:52:57 +0700

    Implement dependency parsing

---
+++ dependency-parsing.py
@@ -0,0 +1,15 @@
+# importing the model en_core_web_sm of English for vocabluary, syntax & entities
+import en_core_web_sm
+
+# load en_core_web_sm of English for vocabluary, syntax & entities
+nlp = en_core_web_sm.load()
+
+docp = nlp (" In pursuit of a wall, President Trump ran into one.")
+
+for chunk in docp.noun_chunks:
+   print(chunk.text, chunk.root.text, chunk.root.dep_,
+          chunk.root.head.text)
+
+from spacy import displacy
+
+displacy.render(docp, style="dep", jupyter=True)
